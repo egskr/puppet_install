4179425f2bd9729c04ae6d7f566bab56f0eb2cf2
Student <student@epbyminw2033.minsk.epam.com>
2017-04-18 20:24:03 +0300

    9

--- manifests/agent_inst.pp
+++ manifests/agent_inst.pp
@@ -1,5 +1,33 @@
 class puppet_install::agent_inst {
      notify { 'Agent': }
+
+
+  exec { 'repo_install':
+    command => "rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm",
+    path => ['/usr/bin', '/usr/sbin',],    
+  }
+
+  package { 'puppet-agent':
+    ensure  => installed,
+    require => Yumrepo['puppet_repo'],
+    require => Exec['repo_install'],
+  }
+
+  yumrepo { 'puppet_repo':
+    ensure   => 'present',
+    baseurl  => "http://yum.puppetlabs.com/el/7/PC1/$basearch",
+    gpgkey   => "https://yum.puppetlabs.com/RPM-GPG-KEY-puppet https://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs",
+    enabled  => '1',
+    gpgcheck => '1'
+  }
+
+   service { 'puppet':
+    ensure  => 'running',
+    enable  => true,
+    require => Package['puppetserver'],
+   }
+
+  
 }
 
 
--- manifests/master_inst.pp
+++ manifests/master_inst.pp
@@ -1,3 +1,37 @@
 class puppet_install::master_inst {
      notify { 'Master': }
+
+
+ file { 'autosign':
+    path    => "/etc/puppetlabs/puppet/autosign.conf",
+    content => template('puppet_install/autosign.erb'),
+ }
+
+  exec { 'repo_install':
+    command => "rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm",
+    path => ['/usr/bin', '/usr/sbin',],    
+  }
+
+  package { 'puppetserver':
+    ensure  => installed,
+    require => Yumrepo['puppet_repo'],
+    require => Exec['puppet_repo'],
+  }
+
+  yumrepo { 'puppet_repo':
+    ensure   => 'present',
+    baseurl  => "http://yum.puppetlabs.com/el/7/PC1/$basearch",
+    gpgkey   => "https://yum.puppetlabs.com/RPM-GPG-KEY-puppet https://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs",
+    enabled  => '1',
+    gpgcheck => '1'
+  }
+
+   service { 'puppetserver':
+    ensure  => 'running',
+    enable  => true,
+    require => Package['puppetserver'],
+}
+
+  
+
 }
